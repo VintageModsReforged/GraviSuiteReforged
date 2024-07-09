8e4ac3b9e9fc7a77cca22aa3fac9b63ce946970f
CrossVas <cross.wanted.1.4@gmail.com>
2024-07-09 15:40:26 +0900

    removed unused entries

--- src/main/java/reforged/mods/gravisuite/proxy/ClientProxy.java
+++ src/main/java/reforged/mods/gravisuite/proxy/ClientProxy.java
@@ -7,9 +7,6 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
@@ -19,11 +16,6 @@ import reforged.mods.gravisuite.GraviSuite;
 import reforged.mods.gravisuite.GraviSuiteData;
 import reforged.mods.gravisuite.GraviSuiteOverlay;
 import reforged.mods.gravisuite.utils.ItemGraviToolRenderer;
-import reforged.mods.gravisuite.utils.Refs;
-
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
@@ -61,21 +53,5 @@ public class ClientProxy extends CommonProxy {
     @ForgeSubscribe
     public void onWorldLoad(WorldEvent.Load event) {
         ClientTickHandler.firstLoad = true;
-    }
-
-    public static void sendPacket(String typePacket, int first_int) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        DataOutputStream data = new DataOutputStream(bytes);
-        try {
-            data.writeUTF(typePacket);
-            data.writeInt(first_int);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Packet250CustomPayload packet = new Packet250CustomPayload();
-        packet.channel = Refs.ID;
-        packet.data = bytes.toByteArray();
-        packet.length = packet.data.length;
-        Minecraft.getMinecraft().thePlayer.sendQueue.addToSendQueue((Packet) packet);
     }
 }
