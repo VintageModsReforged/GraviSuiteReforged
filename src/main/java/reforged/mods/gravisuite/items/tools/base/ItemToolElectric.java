c04693e33aa51b7c8538d91cae04ec170dcb78d0
CrossVas <cross.wanted.1.4@gmail.com>
2024-07-10 09:10:30 +0900

    fixed electric tools breaking when out of energy

--- src/main/java/reforged/mods/gravisuite/items/tools/base/ItemToolElectric.java
+++ src/main/java/reforged/mods/gravisuite/items/tools/base/ItemToolElectric.java
@@ -6,6 +6,7 @@ import ic2.api.item.IElectricItem;
 import ic2.core.IC2;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.EnumToolMaterial;
@@ -13,8 +14,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeEventFactory;
-import reforged.mods.gravisuite.utils.Refs;
 import reforged.mods.gravisuite.utils.Helpers;
+import reforged.mods.gravisuite.utils.Refs;
 import reforged.mods.gravisuite.utils.TextFormatter;
 
 import java.util.List;
@@ -57,6 +58,11 @@ public class ItemToolElectric extends ItemToolBase implements IElectricItem {
     @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean isDebugMode) {
         tooltip.add(TextFormatter.AQUA.literal(Helpers.getCharge(stack) + "/" + this.getMaxCharge(stack) + " EU" + " @ Tier " + this.tier));
+    }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World world, int blockID, int x, int y, int z, EntityLiving user) {
+        return false;
     }
 
     @Override
