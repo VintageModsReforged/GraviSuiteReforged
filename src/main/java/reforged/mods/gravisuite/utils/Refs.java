515a010ca879f590375b1faf5f14ec4014f06549
cross <cross.wanted.1.4@gmail.com>
2024-09-30 15:41:43 +0900

    version index update

--- src/main/java/reforged/mods/gravisuite/utils/Refs.java
+++ src/main/java/reforged/mods/gravisuite/utils/Refs.java
@@ -6,7 +6,7 @@ public class Refs {
 
     public static final String ID = "gravisuite";
     public static final String NAME = "GraviSuite - Addon";
-    public static final String VERSION = "1.4.7-2.2.2n";
+    public static final String VERSION = "1.4.7-2.2.2q";
     public static final String MC_VERSION = "1.4.7";
     public static final String DEPS = "required-after:IC2;after:RedPowerBase;after:AppliedEnergistics;after:GregTech_Addon";
 
